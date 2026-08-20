5cc816ce3439d1e3aff07763b41bb465ec098a4b
myblvddd <bukanputraa@gmail.com>
2026-08-20 23:04:12 +0800

    ade ni js

--- xassignmenttest/feedback.js
+++ xassignmenttest/feedback.js
@@ -1,1 +1,16 @@
 
+
+function submitpopup()
+{
+    var nam = document.forms["fbform"]["fbname"].value;
+    var ema = document.forms["fbform"]["fbmail"].value;
+    var fed = document.forms["fbform"]["fbfb"].value
+    if ( nam != "" && ema != "" && fed != "")
+    {
+        var poppp = document.getElementById("alsopopup");
+        var blurr = document.getElementById("test1")
+        poppp.classList.toggle("show")
+        blurr.classList.toggle("show")
+    }
+
+}
