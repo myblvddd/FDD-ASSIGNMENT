2d12036f48daa50b1cf2a607c37435e1cb11a4a8
myblvddd <bukanputraa@gmail.com>
2026-08-22 22:58:38 +0800

    js sponsor

---
+++ docs/sponsor.js
@@ -0,0 +1,36 @@
+function showdet()
+{
+    var det = document.getElementById("spon");
+    det.classList.toggle("show");
+}
+
+function showdet1()
+{
+    var det = document.getElementById("spon1");
+    det.classList.toggle("show");
+}
+
+function showdet2()
+{
+    var det = document.getElementById("spon2");
+    det.classList.toggle("show");
+}
+
+function showdet3()
+{
+    var det = document.getElementById("spon3");
+    det.classList.toggle("show");
+}
+
+function showdet4()
+{
+    var det = document.getElementById("spon4");
+    det.classList.toggle("show");
+}
+
+function showdet5()
+{
+    var det = document.getElementById("spon5");
+    det.classList.toggle("show");
+}
+
